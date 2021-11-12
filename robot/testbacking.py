7266fcd8d4684fe9c0a67f4ab0fed49aa3c5542c
Jiangwei <17670740485@163.com>
2021-11-12 17:06:28 +0800

    修改了回测函数的表达

--- robot/testbacking.py
+++ robot/testbacking.py
@@ -1,11 +1,19 @@
 from pandas import DataFrame
+from app.utils import getbuy_or_selcsv
 
 
 class Backing(object):
-    # 总余额
     total_account = 1000
     account_ratio = .99
 
     def __init__(self, df: DataFrame):
         self.df = df
+        self.complate()
         return
+
+    def complate(self):
+        df = self.df
+        print(df.describe())
+        for index, row in df.iterrows():
+            if row.buy == 1:
+                row
